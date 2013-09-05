8769b29f04fec66f615e7f0658a784f7fe2048ff
Luke Metz <luke.metz@students.olin.edu>
2013-09-05 15:09:45 -0400

    added buggy code

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,5 +3,13 @@
 
 int32_t main(void) {
     printf("Hello World!\n");
+    uint32_t j = 0;
+    char * k = "Hello world";
+    int i=3;
+    printf("%c",i[k]);
+    if (j=2) {
+    	printf("asdf");
+    };
+
     return 1;
 }
